2dcd31a4f95393ea91e32a5de94bcf8b121eac14
Chris Hughes <chris@speedycomputing.com>
2018-06-15 12:16:37 -0600

    Added Leaderboard Instructions

--- src/edu/cnm/deepdive/Leaderboard.java
+++ src/edu/cnm/deepdive/Leaderboard.java
@@ -3,6 +3,43 @@ package edu.cnm.deepdive;
 /**
  * The static methods of this class compute rankings of an input array of scores
  * against a leaderboard of scores.
+ *
+ * We want to determine Alice's rank as she progresses up the leaderboard. For
+ * example, the four players on the leaderboard have high scores of 100, 90, 90,
+ * and 80. Using dense ranking those players will have ranks 1, 2, 2, and 3
+ * respectively. If Alice's scores are 70, 80 and 105, her rankings after each
+ * game using dense ranking are 4, 3 and 1.
+ *
+ * You are given an array, <code>leaderboard</code>, of monotonically decreasing
+ * leaderboard scores, and another array, <code>scores</code>, of Alice's scores for
+ * the game. You must print j integers. The jth integer should indicate the current
+ * rank of Alice after her jth game.
+ *
+ * <h3>Sample Input</h3>
+ * <pre><code>
+ * 7
+ * 100 100 50 40 40 20 10
+ * 4
+ * 5 25 50 120
+ * </code></pre>
+ *
+ * <h3>Sample Output (Dense Ranking)</h3>
+ * <pre><code>
+ * 6
+ * 4
+ * 2
+ * 1
+ * </code></pre>
+ *
+ * <h3>Sample Output (Competition Ranking)</h3>
+ * <pre><code>
+ * 8
+ * 6
+ * 3
+ * 1
+ * </code></pre>
+ *
+ *
  */
 public class Leaderboard {
 
